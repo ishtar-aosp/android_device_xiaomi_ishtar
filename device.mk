40bbc32119411fcb6743f4b9588e8ca44d1ce9ab
WilliamSado <williamsado1004@gmail.com>
2026-07-19 04:11:23 +0800

    ishtar: Fix speaker audio routing
    Change-Id: I77d887404adc08d8937a9d68f02bd74388f1ad83

--- device.mk
+++ device.mk
@@ -8,6 +8,14 @@
 $(call inherit-product, device/xiaomi/sm8550-common/common.mk)
 
 # Audio
+# The common audio product installs generic kalama MTP files to these same
+# destinations. Drop those entries before installing ishtar's Cirrus/TDM
+# topology so PRODUCT_COPY_FILES de-duplication cannot select the WSA config.
+PRODUCT_COPY_FILES := $(filter-out \
+    %:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/mixer_paths_kalama_mtp.xml \
+    %:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/resourcemanager_kalama_mtp.xml, \
+    $(PRODUCT_COPY_FILES))
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/mixer_paths_kalama_mtp.xml \
     $(LOCAL_PATH)/audio/resourcemanager_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/resourcemanager_kalama_mtp.xml
