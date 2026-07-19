79bcf49f9c28e2fbbdc1a386336d97e388fede11
WilliamSado <williamsado1004@gmail.com>
2026-07-19 18:48:14 +0800

    ishtar: Dezcommon usecaseKvManager.xml
    Change-Id: Id61f9ac3be95be101684e5350711b3d2252adc1d

--- device.mk
+++ device.mk
@@ -18,7 +18,8 @@ PRODUCT_COPY_FILES := $(filter-out \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/mixer_paths_kalama_mtp.xml \
-    $(LOCAL_PATH)/audio/resourcemanager_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/resourcemanager_kalama_mtp.xml
+    $(LOCAL_PATH)/audio/resourcemanager_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/resourcemanager_kalama_mtp.xml \
+    $(LOCAL_PATH)/audio/usecaseKvManager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usecaseKvManager.xml
     
 # Fingerprint
 TARGET_HAS_UDFPS := true
